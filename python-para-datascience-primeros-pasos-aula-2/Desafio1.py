517fab16d1e5c5712bf4cd3f10d991a0e62b138f
Wilmer Mendoza <wmendozar.tipcomps@gmail.com>
2025-04-22 17:43:53 +0200

    desafio 1

--- python-para-datascience-primeros-pasos-aula-2/Desafio1.py
+++ python-para-datascience-primeros-pasos-aula-2/Desafio1.py
@@ -1,6 +1,6 @@
 #1 - Escribe un programa que pida a la persona usuaria que proporcione dos números y muestre el número más grande.
-num1=(int(input("Digite el primer numero: ")))
-num2=(int(input("Digite el segundo numero: ")))
+num1=int(input("Digite el primer numero: "))
+num2=int(input("Digite el segundo numero: "))
 if num1>num2:
     print("El numero mayor es: ",num1)
 elif num2>num1:
