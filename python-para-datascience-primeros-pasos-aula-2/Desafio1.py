0efa3c21fea6e5012f01082256d85c3dc07f9f26
Wilmer Mendoza <wmendozar.tipcomps@gmail.com>
2025-04-22 17:50:14 +0200

    correcion formato print

--- python-para-datascience-primeros-pasos-aula-2/Desafio1.py
+++ python-para-datascience-primeros-pasos-aula-2/Desafio1.py
@@ -2,8 +2,8 @@
 num1=int(input("Digite el primer numero: "))
 num2=int(input("Digite el segundo numero: "))
 if num1>num2:
-    print("El numero mayor es: %d ", %(num1))
+    print("El numero mayor es: %d " %(num1))
 elif num2>num1:
-    print("El numero mayor es: %d ", %(num2))
+    print("El numero mayor es: %d " %(num2))
 else:
     print("Los numeros son iguales")
