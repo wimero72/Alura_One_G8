3ef9d2907a83db46ea834fd78e12270239db1b00
Wilmer Mendoza <wmendozar.tipcomps@gmail.com>
2025-04-22 17:29:00 +0200

    desarrollo del primer desafio

---
+++ python-para-datascience-primeros-pasos-aula-2/Desafio1.py
@@ -0,0 +1,9 @@
+#1 - Escribe un programa que pida a la persona usuaria que proporcione dos números y muestre el número más grande.
+num1=(int(input("Digite el primer numero: ")))
+num2=(int(input("Digite el segundo numero: ")))
+if num1>num2:
+    print("El numero mayor es: ",num1)
+elif num2>num1:
+    print("El numero mayor es: ",num2)
+else:
+    print("Los numeros son iguales")
