8c9c6bbc0aee3f17277d61a659f1ac397787a0a7
Wilmer Mendoza <wmendozar.tipcomps@gmail.com>
2025-04-24 16:47:44 +0200

    completado el desafio 5

--- python-para-datascience-primeros-pasos-aula-2/Desafio5.py
+++ python-para-datascience-primeros-pasos-aula-2/Desafio5.py
@@ -2,4 +2,11 @@
 producto1=float(input("Digite el valor del primer producto : "))
 producto2=float(input("Digite el valor del segundo producto : "))
 producto3=float(input("Digite el valor del tercer producto : "))
-if producto1>
+if producto1<producto2 and producto1<producto3:
+    print("El producto 1 es el mas barato : %.2f" % producto1)
+elif producto2<producto3 and producto2<producto1:
+    print("El producto 2 es el mas barato : %.2f" % producto2)
+elif producto1==producto2 and producto2==producto3:
+    print("Los tres productos tienen el mismo precio : %.2f" % producto1)
+else:
+    print("El producto 3 es el mas barato : %.2f" % producto3)
