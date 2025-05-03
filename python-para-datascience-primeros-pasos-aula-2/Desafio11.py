0903c6328c8d8914d91034d96562b0bbdc832c9e
Wilmer Mendoza <wmendozar.oracle.g8one@gmail.com>
2025-05-03 05:36:53 -0500

    inicio desarrollo desafio 11

---
+++ python-para-datascience-primeros-pasos-aula-2/Desafio11.py
@@ -0,0 +1,8 @@
+#11 - Escribe un programa que pida a la persona usuaria tres números que representan los lados de un triángulo.
+# El programa debe informar si los valores pueden utilizarse para formar un triángulo y, en caso afirmativo, 
+# si es equilátero, isósceles o escaleno. Ten en cuenta algunas sugerencias: 
+#Tres lados forman un triángulo cuando la suma de cualesquiera dos lados es mayor que el tercero; 
+#Triángulo Equilátero: tres lados iguales; 
+#Triángulo Isósceles: dos lados iguales; 
+#Triángulo Escaleno: tres lados diferentes. 
+
